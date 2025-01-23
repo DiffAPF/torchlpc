5e28b302e7aaef8815a7f0474a9d1c1a9c524b50
Chin-Yun Yu <lolimaster.cs03@nctu.edu.tw>
2025-01-23 18:19:19 +0800

    load library file when being imported

--- torchlpc/__init__.py
+++ torchlpc/__init__.py
@@ -1,11 +1,17 @@
 import torch
 from typing import Optional
+from pathlib import Path
 
 from .core import LPC
 from .parallel_scan import WARPSIZE
 from .recurrence import RecurrenceCUDA
 
 __all__ = ["sample_wise_lpc"]
+
+
+so_files = list(Path(__file__).parent.glob("_C*.so"))
+assert len(so_files) == 1, f"Expected one _C*.so file, found {len(so_files)}"
+torch.ops.load_library(so_files[0])
 
 
 def sample_wise_lpc(
